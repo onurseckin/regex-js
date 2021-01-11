eb91acb12d0d2b98e86eb78d35d80776b42e04d9
Onur Seckin Senoglu <onurseckinsenoglu@gmail.com>
2021-01-10 17:55:49 -0800

    14 added

---
+++ 9-match-letters-of-the-alphabet.js
@@ -0,0 +1,3 @@
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-zA-Z]/gi; // Change this line
+let result = quoteSample.match(alphabetRegex); // Change this line
